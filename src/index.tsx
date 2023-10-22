15b53acca2a021750f9f3479b3a29c367e9cbf28
TRLong <vgenygeny@gmail.com>
2023-10-22 14:22:54 +0500

    обернул провайдером

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,17 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
+import { Provider } from 'react-redux';
+import { setupStore } from './store/store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const store = setupStore();
+
 root.render(
-
+  <Provider store={store}>
     <App />
-
+  </Provider>
 );
