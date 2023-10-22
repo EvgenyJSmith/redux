1eab0c8d0490ecd992e5480d093d5c64459d7253
TRLong <vgenygeny@gmail.com>
2023-10-22 14:46:46 +0500

    компонент счетчик

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,9 +1,26 @@
+import { Button } from "antd";
+import { useAppDispatch, useAppSelector } from "../../hooks/redux"
+import { counterSlice } from "../../store/reducers/CounterSlice";
+
+import styles from './Counter.module.scss'
 
 
-const Counter = ()=>{
-    return(
-        <div>
+const Counter = () => {
+    const { count } = useAppSelector(state => state.counerReducer)
+    const dispatch = useAppDispatch()
+    const { decrement, increment, reset } = counterSlice.actions;
+
+    return (
+        <div className={styles.Counter}>
             <h2>Знаменитый счетчик)</h2>
+
+            <span className={styles.number}>{count}</span>
+
+            <div className={styles.buttons}>
+                <Button type="primary"  onClick={() => dispatch(increment(1))}>Increment</Button>
+                <Button type="primary"  onClick={() => dispatch(reset(0))}>Reset</Button>
+                <Button type="primary"  onClick={() => dispatch(decrement(1))}>Decrement</Button>
+            </div>
         </div>
     )
 }
